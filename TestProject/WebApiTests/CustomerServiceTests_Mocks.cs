ed3212fc48a50aba7a2921bf8d4068b110721db0
Daniel Suarez <dansuarmar@gmail.com>
2024-02-02 12:28:05 -0500

    Implemented the same Service Test but using Mock instead of the fake class.
    Mock
    It's a simpler representation of the dependency that you configure to have an especific behavior.
    ```
    //This creates the repository.
    _repository = Substitute.For<ICustomerRepository>();
    //This tells it what to do:
    _repository.GetAllAsync().Returns(Array.Empty<Customer>());
    ```
    NSubstite creates an in memory implementation of what to do, and it's actually called via a Proxy object.
    NSubstitute because Moq requieres too much ceremony, and most experts in C# agree. Also Moq has it's call home issue.

--- TestProject/WebApiTests/CustomerServiceTests_Mocks.cs
+++ TestProject/WebApiTests/CustomerServiceTests_Mocks.cs
@@ -7,21 +7,29 @@ using WebApi.Services;
 using WebApi.Repositories;
 using FluentAssertions;
 using TestProject.WebApiTests.Fakes;
+using NSubstitute;
+using WebApi.Entities;
 
 namespace TestProject.WebApiTests
 {
-    public class CustomerServiceTests
+    public class CustomerServiceTests_Mocks
     {
         private readonly CustomerService _sut;
-        public CustomerServiceTests()
+        private readonly ICustomerRepository _repository;
+
+        public CustomerServiceTests_Mocks()
         {
-            _sut = new CustomerService(new FakeCustomerRepository());
+            //This creates the repository.
+            _repository = Substitute.For<ICustomerRepository>();
+            _sut = new CustomerService(_repository);
         }
 
         [Fact]
         public async Task GetTaskAsync_ShouldReturnEmpty_WhenNoCustomerExist() 
         {
             //Arrange
+            _repository.GetAllAsync().Returns(Array.Empty<Customer>());
+
             //Act
             var customers = await _sut.GetAllAsync();
 
@@ -33,19 +41,25 @@ namespace TestProject.WebApiTests
         public async Task GetTaskAsync_ShouldReturnCustomers_WhenExists() 
         {
             //Arrange
-            await _sut.AddAsync(new WebApi.Entities.Customer 
-            {
-                Name = "name",
-                Id = 1,
-                CreatedDate = DateTime.Now,
-                Credit = 1000m,
-            });
+            Customer[] array = [
+            (
+                new Customer 
+                {
+                    Name = "name",
+                    Id = 1,
+                    CreatedDate = DateTime.Now,
+                    Credit = 1000m,
+                }
+            )];
+            _repository.GetAllAsync().Returns(array);
 
             //Act
             var customers = await _sut.GetAllAsync();
 
             //Assert
             customers.Should().NotBeEmpty();
+            customers.Should().HaveCount(1);
+            customers.Should().ContainSingle(x => x.Name == "name");
         }
     }
 }
