dbbd89ec2d075a94146be9a4fa5519878f50b9ca
Daniel Suarez <dansuarmar@gmail.com>
2024-02-07 12:23:14 -0500

    Understanding the execution model for Class Fixture and CollectionFixture.
    Execution Model
    XUnit creates an instance of the class for each Test. This is so every execution is self contained.
    Each class run its tests in order, but xUnit run every class in parallel. Each class is called a Collection of tests.
    You can modify this behaviors:
    - Class Fixture
    	- If you need something to be the same between al tests. Like a DB initialization or the creation of data that need to be shared between the tess. But remember that the order in which every test is ran, is not guaranteed.
    	- You do this by implementing the interface IClassFixture<Class>, where Class is an aditional class you have to create that will only be instantiated once. The class is then injected into the code.
    - Collection Fixture
    	- You can do the same between collections, but for this you will need to create a CollectionFixture object that works like an anchor point for the data that will be shared.
    	- It implements ICollectionFixture<Class> where class is the Fixture class to share between collections.
    	- The class also have to include the [CollectionDefinition("Name")] attribute, you can put anything as name.
    	- Then, in the test class you have to add the attribute [Collection("Name")].
    	- This cannot be combined with the IClassFixture for the same class, or a new object will be created for each class.

---
+++ TestProject/ClassFixture.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Authentication;
+using Xunit.Abstractions;
+
+namespace TestProject;
+
+public class ClassFixture_Test : IClassFixture<ClassFixture>
+{
+    readonly ClassFixture _classFixture;
+    private readonly ITestOutputHelper _testOutputHelper;
+    readonly Guid Id;
+    public ClassFixture_Test(ITestOutputHelper testOutputHelper, ClassFixture classFixture)
+    {
+        _testOutputHelper = testOutputHelper;
+        _classFixture = classFixture;
+        Id = Guid.NewGuid();
+    }
+
+    [Fact]
+    public void TestClassFixture1()
+    {
+        _testOutputHelper.WriteLine(_classFixture.Id.ToString());
+        _testOutputHelper.WriteLine(Id.ToString());
+        Id.Should().NotBe(_classFixture.Id);
+    }
+
+    [Fact]
+    public void TestClassFixture2()
+    {
+        //This should be the same in both tests.
+        _testOutputHelper.WriteLine(_classFixture.ToString());
+        //This should be different in both tests.
+        _testOutputHelper.WriteLine(Id.ToString());
+        Id.Should().NotBe(_classFixture.Id);
+    }
+}
+
+public class ClassFixture
+{
+    public Guid Id = Guid.NewGuid();
+}
---
+++ TestProject/CollectionFixture.cs
@@ -0,0 +1,77 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Authentication;
+using Xunit.Abstractions;
+
+namespace TestProject;
+
+[Collection("CollectionName")]
+public class CollectionFixture_Test
+{
+    readonly ClassFixture4Collection _classFixture;
+    private readonly ITestOutputHelper _testOutputHelper;
+    readonly Guid Id;
+    public CollectionFixture_Test(ITestOutputHelper testOutputHelper, ClassFixture4Collection classFixture)
+    {
+        _testOutputHelper = testOutputHelper;
+        _classFixture = classFixture;
+        Id = Guid.NewGuid();
+    }
+
+    [Fact]
+    public void TestCollectionFixture11()
+    {
+        _testOutputHelper.WriteLine(_classFixture.Id.ToString());
+        _testOutputHelper.WriteLine(Id.ToString());
+        Id.Should().NotBe(_classFixture.Id);
+    }
+
+    [Fact]
+    public void TestCollectionFixture12()
+    {
+        //This should be the same in both tests.
+        _testOutputHelper.WriteLine(_classFixture.Id.ToString());
+        //This should be different in both tests.
+        _testOutputHelper.WriteLine(Id.ToString());
+        Id.Should().NotBe(_classFixture.Id);
+    }
+}
+
+[Collection("CollectionName")]
+public class CollectionFixture2_Test
+{
+    readonly ClassFixture4Collection _classFixture;
+    private readonly ITestOutputHelper _testOutputHelper;
+    readonly Guid Id;
+    public CollectionFixture2_Test(ITestOutputHelper testOutputHelper, ClassFixture4Collection classFixture)
+    {
+        _testOutputHelper = testOutputHelper;
+        _classFixture = classFixture;
+        Id = Guid.NewGuid();
+    }
+
+    [Fact]
+    public void TestCollectionFixture21()
+    {
+        _testOutputHelper.WriteLine(_classFixture.Id.ToString());
+        _testOutputHelper.WriteLine(Id.ToString());
+        Id.Should().NotBe(_classFixture.Id);
+    }
+
+    [Fact]
+    public void TestCollectionFixture22()
+    {
+        //This should be the same in both tests.
+        _testOutputHelper.WriteLine(_classFixture.Id.ToString());
+        //This should be different in both tests.
+        _testOutputHelper.WriteLine(Id.ToString());
+        Id.Should().NotBe(_classFixture.Id);
+    }
+}
+
+public class ClassFixture4Collection
+{
+    public Guid Id = Guid.NewGuid();
+}
+
+[CollectionDefinition("CollectionName")]
+public class CollectionFixture : ICollectionFixture<ClassFixture4Collection>{}
