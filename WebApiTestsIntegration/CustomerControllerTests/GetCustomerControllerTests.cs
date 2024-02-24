6057cecd7410f92a77525738a601661ddf6b0aa3
Daniel Suarez <dansuarmar@gmail.com>
2024-02-24 11:46:36 -0500

    You might want to consider creating one test class per HTTP command. We created one class per each.

---
+++ WebApiTestsIntegration/CustomerControllerTests/GetCustomerControllerTests.cs
@@ -0,0 +1,59 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Testing;
+using WebApi;
+
+namespace WebApiTestsIntegration;
+
+public class GetCustomerControllerTests : IClassFixture<WebApplicationFactory<IApiMarker>>
+{
+    private readonly WebApplicationFactory<IApiMarker> _appFactory;
+    private readonly HttpClient _httpClient;
+    public GetCustomerControllerTests(WebApplicationFactory<IApiMarker> appFactory)
+    {
+        _appFactory = appFactory;
+        _httpClient = _appFactory.CreateClient();
+    }
+
+            [Fact]
+        public async Task Get_ReturnsCustomer_WhenCustomerExists()
+        {
+            //Arrange
+            //Act
+            var response = await _httpClient.GetAsync($"/api/customer/3");
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            //Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Get_ReturnsNotFound_WhenCurtomerDoesNotExists() 
+        {
+            //Arrange
+            //Act
+            var response = await _httpClient.GetAsync($"/api/customer/4");
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+            //Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+            
+            //Check the Content.
+            //This is to read it as text. But for a test is too simple.
+            var responseText = await response.Content.ReadAsStringAsync();
+            responseText.Should().Contain("404");
+            //You can also read it as an object.
+            var problem = await response.Content.ReadFromJsonAsync<ValidationProblemDetails>();
+            problem!.Title.Should().Be("Not Found");
+            problem.Status.Should().Be(404);
+
+            //Check the Headers
+            //For standard headers.
+            response!.Headers!.Location!.Should().BeNull();
+            //For non standard headers.
+            
+            //response.Headers.GetValues("Location").Should().BeNull();
+        }
+}
---
+++ WebApiTestsIntegration/CustomerControllerTests/PostCustomerControllerTests.cs
@@ -0,0 +1,34 @@
+﻿
+using Bogus;
+using Microsoft.AspNetCore.Mvc.Testing;
+using WebApi;
+using WebApi.Entities;
+
+namespace WebApiTestsIntegration;
+
+public class PostCustomerControllerTests : IClassFixture<WebApplicationFactory<IApiMarker>>, IAsyncLifetime
+{
+    private readonly WebApplicationFactory<IApiMarker> _appFactory;
+    private readonly HttpClient _httpClient;
+    private readonly Faker<Customer> _fakeCustomerGenetator = new Faker<Customer>()
+        .RuleFor(m => m.Name, faker => faker.Person.FullName)
+        .RuleFor(m => m.Id, faker => faker.Database.Random.Int(4))
+        .RuleFor(m => m.Credit, faker => faker.Finance.Random.Decimal())
+        .RuleFor(m => m.CreatedDate, DateTime.Now)
+        ;
+    private readonly List<int> _createdIds;
+
+    public PostCustomerControllerTests(WebApplicationFactory<IApiMarker> appFactory)
+    {
+        _appFactory = appFactory;
+        _httpClient = _appFactory.CreateClient();
+        _createdIds = [];
+    }
+
+    public Task InitializeAsync() => Task.CompletedTask;
+    public async Task DisposeAsync()
+    {
+        foreach (var customerId in _createdIds)
+            await _httpClient.DeleteAsync($"/api/customer/{customerId}");
+    }
+}
